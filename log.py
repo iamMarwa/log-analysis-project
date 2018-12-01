27aa463f9a96d94a6f61e03c0719b62cd1e5647e
vagrant <vagrant@vagrant.vm>
2018-12-01 12:43:35 +0000

    last change

--- log.py
+++ log.py
@@ -1,55 +1,67 @@
-#!/usr/bin/env python3
+
+
+# !/usr/bin/env python3
 import psycopg2
 import time
+
+
 def connect():
     return psycopg2.connect("dbname=news")
 
-query1="SELECT title, count(path) AS num FROM articles, log WHERE articles.slug = substring(path from 10 for 100) GROUP BY title ORDER BY num DESC LIMIT 3;"
-query2="SELECT a.name, p.num \
+
+query1 = "SELECT title, count(path) AS num FROM articles, log \
+ WHERE articles.slug = substring(path from 10 for 100) GROUP BY \
+ title ORDER BY num DESC LIMIT 3;"
+query2 = "SELECT a.name, p.num \
                  FROM authors AS a\
                  INNER JOIN mostAuthor AS p ON p.author = a.id \
                  GROUP BY a.name, p.num \
                  ORDER BY p.num DESC;"
-query3="select to_char(date,'Mon DD,YYYY') as date,error_result from error_result where error_result>1.0;"
+query3 = "select to_char(date,'Mon DD,YYYY') as date,error_result \
+ from error_result where error_result>1.0;"
+
 
 def most_article(query1):
-    db=connect()
-    c=db.cursor()
+    db = connect()
+    c = db.cursor()
     c.execute(query1)
-    results=c.fetchall()
+    results = c.fetchall()
     for i in range(len(results)):
-        title=results[i][0]
-        views=results[i][1]
-        print("%s -- %d views" % (title,views))
+        title = results[i][0]
+        views = results[i][1]
+        print("%s -- %d views" % (title, views))
     db.close()
+
 
 def most_authors(query2):
-    db=connect() 
-    c=db.cursor()
+    db = connect()
+    c = db.cursor()
     c.execute(query2)
-    results=c.fetchall()
+    results = c.fetchall()
     for i in range(len(results)):
-        name=results[i][0]
-        views=results[i][1]
-        print("%s -- %d views" % (name,views))
+        name = results[i][0]
+        views = results[i][1]
+        print("%s -- %d views" % (name, views))
     db.close()
 
+
 def error_result(query3):
-    db=connect()
-    c=db.cursor()
+    db = connect()
+    c = db.cursor()
     c.execute(query3)
-    results=c.fetchall()
+    results = c.fetchall()
     for i in range(len(results)):
-        date=results[i][0]
-        err_prc=results[i][1]
-        print("%s -- %.1f %%" %(date,err_prc))
+        date = results[i][0]
+        err_prc = results[i][1]
+        print("%s -- %.1f %%" % (date, err_prc))
+
 
 if __name__ == "__main__":
-  print("What are the most popular three articles of all time?")
-  most_article(query1)
-  print("\n")
-  print("Who are the most popular article authors of all time?")
-  most_authors(query2)
-  print("\n")
-  print("On which days did more than 1% of requests lead to errors?")
-  error_result(query3)
+    print("What are the most popular three articles of all time?")
+    most_article(query1)
+    print("\n")
+    print("Who are the most popular article authors of all time?")
+    most_authors(query2)
+    print("\n")
+    print("On which days did more than 1% of requests lead to errors?")
+    error_result(query3)
