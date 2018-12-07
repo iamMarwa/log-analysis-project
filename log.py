b49a31efa1b9d105652eafae59f6ce8cb34281db
vagrant <vagrant@vagrant.vm>
2018-12-07 15:49:25 +0000

    Edit log.py file

--- log.py
+++ log.py
@@ -1,14 +1,21 @@
-
-
 # !/usr/bin/env python3
 import psycopg2
 import time
 
 
-def connect():
-    return psycopg2.connect("dbname=news")
+def connect(news):
+    try:
+        db = psycopg2.connect("dbname={}".format(news))
+    except psycopg2.Error as e:
+        print("Unable to connect!")
+        print(e.pgerror)
+        print(e.diag.message_detail)
+        sys.exit(1)
+    cursor = db.cursor()
+    return db, cursor
 
 
+db, cursor = connect("news")
 query1 = "SELECT title, count(path) AS num FROM articles, log \
  WHERE articles.slug = substring(path from 10 for 100) GROUP BY \
  title ORDER BY num DESC LIMIT 3;"
@@ -24,34 +31,34 @@ query3 = "SELECT to_char(err.most_day, 'MM/DD/YYYY'),\
 
 
 def most_article(query1):
-    db = connect()
-    c = db.cursor()
-    c.execute(query1)
-    results = c.fetchall()
+    # db = connect()
+    cursor = db.cursor()
+    cursor.execute(query1)
+    results = cursor.fetchall()
     for i in range(len(results)):
         title = results[i][0]
         views = results[i][1]
         print("%s -- %d views" % (title, views))
-    db.close()
+    # db.close()
 
 
 def most_authors(query2):
-    db = connect()
-    c = db.cursor()
-    c.execute(query2)
-    results = c.fetchall()
+    # db = connect()
+    cursor = db.cursor()
+    cursor.execute(query2)
+    results = cursor.fetchall()
     for i in range(len(results)):
         name = results[i][0]
         views = results[i][1]
         print("%s -- %d views" % (name, views))
-    db.close()
+    # db.close()
 
 
 def error_result(query3):
-    db = connect()
-    c = db.cursor()
-    c.execute(query3)
-    results = c.fetchall()
+    # db = connect()
+    cursor = db.cursor()
+    cursor.execute(query3)
+    results = cursor.fetchall()
     for i in range(len(results)):
         date = results[i][0]
         err_prc = results[i][1]
