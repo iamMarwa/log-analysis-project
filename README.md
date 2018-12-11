e0cbd38aa5af95228ecaae654e582173d1a95d4c
vagrant <vagrant@vagrant.vm>
2018-12-11 15:59:21 +0000

    change log.py file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 # Logs-analysis
-This project is part of the Full Stack Web Developer nanodegree from udacity.
+This project is for the Full Stack Web Developer nanodegree from udacity.
 
 ## Prerequirements
 * [Python3](https://www.python.org/downloads/)
@@ -8,13 +8,20 @@ This project is part of the Full Stack Web Developer nanodegree from udacity.
 * [Vagrant](https://www.vagrantup.com/downloads.html)
 
 ## How To Run The Program:
-1. Download or clone repository on you machine.
-2. Bring the project directory under the vagrant directory.
-3. Start the virtual machine using vagrant up
-command.
-4. After that, run vagrant ssh log in to your VM.
-5. Download the database from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
-6. To load the data, cd into the vagrant directory and use the command:
+1. Download or clone the project on your machine under the vagrant directory by using:
+```
+git clone https://github.com/iamMarwa/log-analysis-project.git
+```
+2. Start the virtual machine by using this command under the vagrant directory:
+```
+vagrant up
+```
+3. Then to log in to your virtual machine you'll need to use this command:
+```
+vagrant ssh
+```
+4. Download the database from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+5. To load the data, cd into the vagrant directory and use the command:
 ```
 psql -d news -f newsdata.sql
 ```
@@ -22,24 +29,18 @@ Use this command in order to access the database:
 ```
 psql news
 ```
-7. Go to log-analysis-project directory with
-cd /vagrant/log-analysis-project
-8. Run log.py file to analyse the log data from the database using
+6. Go to the project directory by using:
+```
+cd log-analysis-project
+```
+7. To run the project and see the output run:
 ```
 python3 log.py
 ```
-9. Go to output.txt file to discover what kind of article's the site's readers like.
-10. Shutdown the VM with CTRL + D
 
  ## SQL View:
 
 The following SQL view are required to run the program. Run psql news inside the /vagrant directory and either copy paste them individually or type them manually.
-```
-CREATE VIEW mostAuthor AS SELECT author, count(log.path) AS num FROM articles, log WHERE articles.slug = substring(path from 10 for 100) GROUP BY author ORDER BY num DESC LIMIT 4;
-```
-
-This statements mean that "Create view to find relationships between authors and relevant logs"
-
 
 ```
 CREATE VIEW total_re AS SELECT DATE(time) AS most_day, COUNT(status) AS total FROM log GROUP BY most_day ORDER BY most_day;
--- log.py
+++ log.py
@@ -1,76 +1,67 @@
-# !/usr/bin/env python3
+# !/usr/bin/env python
+
 import psycopg2
 import time
 
+DBNAME = "news"
 
-def connect(news):
-    try:
-        db = psycopg2.connect("dbname={}".format(news))
-    except psycopg2.Error as e:
-        print("Unable to connect!")
-        print(e.pgerror)
-        print(e.diag.message_detail)
-        sys.exit(1)
-    cursor = db.cursor()
-    return db, cursor
+questionOne = "What are the most popular three articles of all time?"
 
+queryOne = ("SELECT title, COUNT(title) AS num FROM \
+articles INNER JOIN log ON log.path LIKE CONCAT('/article/', articles.slug) \
+GROUP BY log.path, articles.title ORDER BY num DESC LIMIT 3;")
 
-db, cursor = connect("news")
-query1 = "SELECT title, count(path) AS num FROM articles, log \
- WHERE articles.slug = substring(path from 10 for 100) GROUP BY \
- title ORDER BY num DESC LIMIT 3;"
-query2 = "SELECT a.name, p.num \
-                 FROM authors AS a\
-                 INNER JOIN mostAuthor AS p ON p.author = a.id \
-                 GROUP BY a.name, p.num \
-                 ORDER BY p.num DESC;"
-query3 = "SELECT to_char(err.most_day, 'MM/DD/YYYY'),\
+questionTwo = "Who are the most popular article authors of all time?"
+
+queryTwo = ("SELECT authors.name, COUNT(*) AS num\
+ FROM articles INNER JOIN authors \
+ON articles.author = authors.id \
+INNER JOIN log \
+ON log.path = CONCAT('/article/', articles.slug)\
+GROUP BY authors.name\
+ ORDER BY num DESC\
+ LIMIT 4;")
+
+questionThree = "On which days did more than 1% of requests lead to errors?"
+
+queryThree = ("SELECT to_char(err.most_day, 'MM/DD/YYYY'),\
  round((err.errors*1.0 / total_re.total*1.0)*100, 2) as per \
   FROM err, total_re WHERE err.most_day = total_re.most_day and \
-   (err.errors*1.0 / total_re.total*1.0)*100 > 1 ORDER BY per desc;"
+   (err.errors*1.0 / total_re.total*1.0)*100 > 1 ORDER BY per desc;")
 
 
-def most_article(query1):
-    # db = connect()
-    cursor = db.cursor()
-    cursor.execute(query1)
-    results = cursor.fetchall()
-    for i in range(len(results)):
-        title = results[i][0]
-        views = results[i][1]
-        print("%s -- %d views" % (title, views))
-    # db.close()
+def results(query):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
 
 
-def most_authors(query2):
-    # db = connect()
-    cursor = db.cursor()
-    cursor.execute(query2)
-    results = cursor.fetchall()
-    for i in range(len(results)):
-        name = results[i][0]
-        views = results[i][1]
-        print("%s -- %d views" % (name, views))
-    # db.close()
+question1 = results(queryOne)
+question2 = results(queryTwo)
+question3 = results(queryThree)
 
 
-def error_result(query3):
-    # db = connect()
-    cursor = db.cursor()
-    cursor.execute(query3)
-    results = cursor.fetchall()
-    for i in range(len(results)):
-        date = results[i][0]
-        err_prc = results[i][1]
-        print("%s -- %.1f %%" % (date, err_prc))
-
-
-if __name__ == "__main__":
-    print("What are the most popular three articles of all time?")
-    most_article(query1)
+def printTheResult(result):
+    for i in range(len(result)):
+        title = result[i][0]
+        views = result[i][1]
+        print("\t" + "%s - %d" % (title, views) + " views")
     print("\n")
-    print("Who are the most popular article authors of all time?")
-    most_authors(query2)
-    print("\n")
-    print("On which days did more than 1% of requests lead to errors?")
-    error_result(query3)
+
+
+def printquestion3(resultt):
+    for i in range(len(resultt)):
+        date = resultt[i][0]
+        prc = resultt[i][1]
+        print("%s -- %.1f %%" % (date, prc))
+
+
+print(questionOne)
+printTheResult(question1)
+print(questionTwo)
+printTheResult(question2)
+print(questionThree)
+printquestion3(question3)
